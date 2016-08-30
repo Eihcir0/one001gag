43557bf20ee3c098a667a417bf8fef00479dc414
Richie Humphrey <Eihcir0@Richies-MacBook-Air.local>
2016-08-30 09:21:50 -0700

    test

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,6 +3,6 @@ import { Provider } from 'react-redux';
 
 export const Root = ({ store }) => (
   <Provider store={store}>
-    <h1> one001gag</h1>
+    <h1> TEST!!!one001gag</h1>
   </Provider>
 );
