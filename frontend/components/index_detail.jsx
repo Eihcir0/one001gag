c7e25fe425ce90a78f7f9b489f24a8959a49ed40
Eihcir0 <eihcir0@gmail.com>
2016-09-01 20:27:23 -0700

    goodnight

--- frontend/components/index_detail.jsx
+++ frontend/components/index_detail.jsx
@@ -7,23 +7,54 @@ class IndexDetail extends React.Component {
   constructor(props){
     super(props);
     this.postId = this.props.params.id;
-    // debugger;
+    this.commentBody = "";
   }
 
   componentDidMount() {
-    // debugger;
     this.props.requestPost(this.postId);
   }
 
+  update(property) {
+    return e => this.setState({[property]: e.target.value});
+  }
+  handleCommentSubmit () {
+    return ()=>true;
+  }
 
   render() {
-    // debugger;
     let post = this.props.posts.posts[this.postId];
     return (
-      <section className="index-detail-page">
+        <div className="index-detail-page">
         <NavbarContainer/>
+        <section className="index-detail-container">
         <IndexItem key={this.postId} post={post} />
+        <div className="create-post-form">
+            <form className="new-comment-form" onSubmit={this.handleCommentSubmit()}>
+              <div className="comment-body">
+                <textarea
+                  className="comment-text-input"
+                  ref="body"
+                  cols='60'
+                  value={this.commentBody ? this.commentBody : "Write comment..."}
+                  rows='5'
+                  onChange={this.update('commentBody')}></textarea>
+              </div>
+          <button className="post-comment-button">Post</button>
+        </form>
+      </div>
+      <br/>
+      <div className="comments-items">
+      <p>Jose: 1st comment!!!</p>
+      <br/>
+        <p>John B: This post sucks!</p>
+
+        <br/>
+          <p>Joe B: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+      </div>
       </section>
+    </div>
     );
   }
 }
